dfc369b58154bcd968bf4c76ad3baa796050c714
SquidHat14 <tsmitty0614@gmail.com>
2021-08-22 23:35:34 -0400

    crimple

--- server-side/readFiles.js
+++ server-side/readFiles.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const mysql = require('mysql');
 const app = express();
 const cors = require('cors');
 const fs = require('fs');
--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import Title from './Title/Title';
 import Navbar from './Navbar/Navbar';
+import Login from './Login/Login';
 import { Component } from "react";
 import React from "react";
 import './_App.scss';
+import './_vars.scss';
 import './fonts.scss';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 class App extends Component {  
 
@@ -22,8 +30,21 @@ class App extends Component {
   {
     return (
       <div>
-      <Title />
-      <Navbar />
+        <Router>
+          
+
+
+          <Switch>
+          <Route path="/login">
+                          <Login />
+                        </Route>
+
+          <Route path="/">
+                        <Title />
+                        <Navbar />
+                        </Route>
+          </Switch>
+      </Router>
       </div>
     );
   }
